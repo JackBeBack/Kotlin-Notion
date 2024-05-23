a6aebc5450e39a0d815b89ff9b68bd1eed12eae1
jack <kloos.jonas@googlemail.com>
2024-05-23 09:48:20 +0200

    Hello World

---
+++ src/main/kotlin/jackbeback/de/Application.kt
@@ -0,0 +1,11 @@
+package jackbeback.de
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import jackbeback.de.plugins.*
+
+fun main() {
+    println("Hello World")
+}
+
